da7d0314db64c6132b4730b3a58785a56ae667d5
Tomas Perez <tomwpv@gmail.com>
2020-02-20 16:03:32 -0500

    Add code for #caesar_cipher

--- caesar_cipher/lib/caesar_cipher.rb
+++ caesar_cipher/lib/caesar_cipher.rb
@@ -1,2 +1,23 @@
-def caesar_cipher
+def caesar_cipher(string, shift_factor)
+  return '' if string.empty?
+
+  alphabet = ('a'..'z').to_a
+
+  chars = string.downcase.strip.split('')
+
+  positions = chars.map do |letter|
+    char = alphabet.index(letter)
+
+    char.nil? ? letter : (char + shift_factor) % 26
+  end
+
+  modified_string = positions.map.with_index do |position, index|
+    if position.is_a? String
+      position
+    else
+      string[index].match?(/[A-Z]/) ? alphabet[position].upcase : alphabet[position]
+    end
+  end
+
+  modified_string.join
 end
