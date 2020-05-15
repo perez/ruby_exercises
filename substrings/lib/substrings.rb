f84ef30a34fa804218894d0b459598afa2353e0f
Tomas Perez <tomwpv@gmail.com>
2020-05-15 18:34:02 -0400

    Add method #substrings

--- substrings/lib/substrings.rb
+++ substrings/lib/substrings.rb
@@ -1,1 +1,13 @@
+def substrings(str, arr)
+  str_arr = str.downcase.split
 
+  substr_hash = Hash.new(0)
+
+  arr.each do |arr_word|
+    str_arr.each do |str_word|
+      substr_hash[arr_word] += 1 if str_word.include?(arr_word)
+    end
+  end
+
+  substr_hash
+end
