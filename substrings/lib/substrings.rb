37f6df48de7e1eb610d9dc361b4692bd6b595a2f
Tomas Perez <tomwpv@gmail.com>
2020-05-20 21:32:40 -0400

    Refactor #substrings method

--- substrings/lib/substrings.rb
+++ substrings/lib/substrings.rb
@@ -1,10 +1,8 @@
-def substrings(str, arr)
-  str_arr = str.downcase.split
-
+def substrings(string, arr)
   substr_hash = Hash.new(0)
 
   arr.each do |arr_word|
-    str_arr.each do |str_word|
+    string.downcase.split.each do |str_word|
       substr_hash[arr_word] += 1 if str_word.include?(arr_word)
     end
   end
