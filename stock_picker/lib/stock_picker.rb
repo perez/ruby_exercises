4f81e15fcbb64920a835554c8d5e6c25db89a29d
Tomas Perez <tomwpv@gmail.com>
2020-05-28 17:08:11 -0400

    Add code for #stock_picker

--- stock_picker/lib/stock_picker.rb
+++ stock_picker/lib/stock_picker.rb
@@ -1,1 +1,27 @@
+def stock_picker(stock_prices)
+  result = stock_prices.dup
 
+  buy_day = result.min
+
+  sell_day = result.max
+
+  loop do
+    if result.last == result.min
+      result.delete(buy_day)
+
+      buy_day = result.min
+    elsif result.first == result.max
+      result.delete(sell_day)
+
+      sell_day = result.max
+    elsif result.index(buy_day) > result.index(sell_day)
+      result.delete(buy_day)
+  
+      buy_day = result.min
+    else
+      break
+    end
+  end
+  
+  [stock_prices.index(buy_day), stock_prices.index(sell_day)]
+end
