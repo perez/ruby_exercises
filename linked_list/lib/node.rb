79624023662a8d04d8193afb0f40464fde5de694
Tomas Perez <tomwpv@gmail.com>
2020-06-16 17:29:31 -0400

    Update Node class to be able to accept a next_node argument when initializing

--- linked_list/lib/node.rb
+++ linked_list/lib/node.rb
@@ -1,8 +1,8 @@
 class Node
   attr_accessor :node, :next_node
 
-  def initialize(node = nil)
+  def initialize(node = nil, next_node = nil)
     @node = node
-    @next_node = nil
+    @next_node = next_node
   end
 end
