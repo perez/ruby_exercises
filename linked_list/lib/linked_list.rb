a7cbb1eeb257fc9bc4a7d74570f73f4e08943813
Tomas Perez <tomwpv@gmail.com>
2020-06-16 21:36:20 -0400

    Add method #removed_at and refactor most methods

--- linked_list/lib/linked_list.rb
+++ linked_list/lib/linked_list.rb
@@ -1,18 +1,20 @@
 class LinkedList
-  attr_reader :head, :tail
-
   def initialize
     @head = nil
     @tail = nil
   end
 
-  def append(node)
+  def append(value)
+    node = Node.new(value)
+
     @head.nil? ? @head = node : @tail.next_node = node
 
     @tail = node
   end
 
-  def prepend(node)
+  def prepend(value)
+    node = Node.new(value)
+
     if @head.nil?
       @head = node
     
@@ -25,61 +27,65 @@ class LinkedList
   end
 
   def size
+    current_node = @head
+    
     size = 0
 
-    current_node = @head
-
     until current_node.nil?
-      size += 1
-
       current_node = current_node.next_node
+      
+      size += 1
     end
 
     size
   end
 
-  # def head
-  #   @head.nil? ? nil : @head.node
-  # end
+  def head
+    @head.nil? ? nil : @head.node
+  end
 
-  # def tail
-  #   @tail.nil? ? nil : @tail.node
-  # end
+  def tail
+    @tail.nil? ? nil : @tail.node
+  end
 
   def at(index)
+    return nil if index >= self.size
+    
     current_node = @head
 
-    1.upto(index) do
+    index.times do
       current_node = current_node.next_node
       
       break if current_node.nil?
     end
 
-    puts current_node.nil? ? nil : current_node.node
+    current_node.node
   end
 
   def pop
+    return nil if @head.nil?
+
     current_node = @head
 
-    until current_node.next_node == @tail
-      current_node = current_node.next_node
-    end
+    popped = @tail.node
+
+    current_node = current_node.next_node until current_node.next_node == @tail
 
     current_node.next_node = nil
 
     @tail = current_node
+
+    popped
   end
 
   def contain?(value)
     current_node = @head
 
-    until current_node.next_node.nil?
-      break if current_node.node == value
-
-      current_node = current_node.next_node
+    until current_node.next_node.nil?      
+      current_node.node == value ? break : current_node = current_node.next_node
     end
     
-    puts current_node.node == value ? true : false 
+    current_node.node == value
   end
 
   def find(value)
@@ -88,14 +94,12 @@ class LinkedList
     index = 0
 
     until current_node.nil?
-      break if current_node.node == value
-
-      current_node = current_node.next_node
+      current_node.node == value ? break : current_node = current_node.next_node
 
       index += 1
     end
 
-    puts current_node.nil? ? 'nil' : index
+    current_node.nil? ? nil : index
   end
 
   def to_s
@@ -115,12 +119,44 @@ class LinkedList
   def insert_at(value, index)
     current_node = @head
 
-    1.upto(index) do
-      current_node = current_node.next_node
-      
-      break if current_node.nil?
+    previous_node = nil
+
+    if index.zero?
+      self.prepend(value)
+    elsif index >= self.size
+      nil
+    else
+      index.times do
+        previous_node = current_node
+  
+        current_node = current_node.next_node
+      end
+  
+      previous_node.next_node = Node.new(value, current_node)
+    end
+  end
+
+  def remove_at(index)
+    removed = self.at(index)
+    
+    current_node = @head
+
+    previous_node = nil
+
+    if index.zero?
+      @head = @head.next_node
+    elsif index >= self.size
+      nil
+    else
+      index.times do
+        previous_node = current_node
+  
+        current_node = current_node.next_node
+      end
+  
+      previous_node.next_node = current_node.next_node
     end
 
-    current_node.node = value unless current_node.nil?
+    removed
   end
 end
