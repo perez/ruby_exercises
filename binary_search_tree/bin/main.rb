d98074f78375329fc03355c4f4e531487336bbd4
Tomas Perez <tomwpv@gmail.com>
2020-07-20 22:27:08 -0400

    Add driver script that showcases the most important methods of class Tree

--- binary_search_tree/bin/main.rb
+++ binary_search_tree/bin/main.rb
@@ -1,1 +1,34 @@
+require_relative '../lib/node'
+require_relative '../lib/tree'
 
+tree = Tree.new(Array.new(15) { rand(1..100) })
+
+puts "Balanced: #{tree.balanced?}"
+
+puts "Level Order: #{tree.level_order}"
+
+puts "Preorder: #{tree.depth_order(:preorder)}"
+
+puts "Inorder: #{tree.depth_order(:inorder)}"
+
+puts "Postorder: #{tree.depth_order(:postorder)}"
+
+tree.insert(Node.new(115))
+
+tree.insert(Node.new(137))
+
+puts "Balanced: #{tree.balanced?}"
+
+puts "Rebalancing tree..."
+
+tree.rebalance
+
+puts "Balanced: #{tree.balanced?}"
+
+puts "Level Order: #{tree.level_order}"
+
+puts "Preorder: #{tree.depth_order(:preorder)}"
+
+puts "Inorder: #{tree.depth_order(:inorder)}"
+
+puts "Postorder: #{tree.depth_order(:postorder)}"
