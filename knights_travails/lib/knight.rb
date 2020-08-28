b6872073f6fab4313a0a22c17897a6a9ea10707d
Tomas Perez <tomtommyrez@gmail.com>
2020-08-27 21:43:47 -0400

    Add class Knight with method possible_moves, which calculates all the first moves a node can make from a point on the board

---
+++ knights_travails/lib/knight.rb
@@ -0,0 +1,31 @@
+class Knight
+  attr_accessor :piece, :end_point, :root
+
+  def initialize(start_point, end_point)
+    @piece = '♞'
+    @end_point = Node.new(end_point)
+    @root = Node.new(start_point)
+  end
+
+  def possible_moves(node)
+    x = node.point.last
+
+    y = node.point.first
+
+    node.moves << Node.new([y - 1, x - 2]) unless (y - 1 < 0) || (x - 2 < 0)
+
+    node.moves << Node.new([y - 2, x - 1]) unless (y - 2 < 0) || (x - 1 < 0)
+
+    node.moves << Node.new([y - 2, x + 1]) unless (y - 2 < 0) || (x + 1 > 7)
+
+    node.moves << Node.new([y - 1, x + 2]) unless (y - 1 < 0) || (x + 2 > 7)
+
+    node.moves << Node.new([y + 1, x + 2]) unless (y + 1 > 7) || (x + 2 > 7)
+
+    node.moves << Node.new([y + 2, x + 1]) unless (y + 2 > 7) || (x + 1 > 7)
+
+    node.moves << Node.new([y + 2, x - 1]) unless (y + 2 > 7) || (x - 1 < 0)
+
+    node.moves << Node.new([y + 1, x - 2]) unless (y + 1 > 7) || (x - 2 < 0)
+  end
+end
