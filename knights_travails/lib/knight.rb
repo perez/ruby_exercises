5fea74b7d99cac881b8fc00f568cb73d0cc62229
Tomas Perez <tomtommyrez@gmail.com>
2020-08-29 19:01:29 -0400

    Add #possible_paths, which will generate a tree like structure of nodes until one of the nodes is the end point

--- knights_travails/lib/knight.rb
+++ knights_travails/lib/knight.rb
@@ -5,7 +5,10 @@ class Knight
     @piece = '♞'
     @end_point = Node.new(end_point)
     @root = Node.new(start_point)
+    @root == @end_point ? return : possible_paths(@root)
   end
+
+  private
 
   def possible_moves(node)
     x = node.point.last
@@ -27,5 +30,29 @@ class Knight
     node.moves << Node.new([y + 2, x - 1]) unless (y + 2 > 7) || (x - 1 < 0)
 
     node.moves << Node.new([y + 1, x - 2]) unless (y + 1 > 7) || (x - 2 < 0)
+  end
+
+  def possible_paths(node)
+    possible_moves(node)
+
+    return if node.moves.include?(@end_point)
+
+    current_nodes = node.moves
+
+    loop do
+      next_nodes = []
+
+      current_nodes.map do |child|
+        possible_moves(child)
+
+        return if child.moves.include?(@end_point)
+      end
+
+      current_nodes.each do |child|
+        child.moves.each { |move| next_nodes << move }
+      end
+
+      current_nodes = next_nodes
+    end
   end
 end
