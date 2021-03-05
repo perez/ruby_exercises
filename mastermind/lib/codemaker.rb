366d5208ca09d528e613f5155ef9d1d78d86b09d
perez <tomwpv@gmail.com>
2021-03-04 20:28:28 -0500

    Add methods to Codemaker that takes Codebreaker guess and provides feeback on the accuracy of the guess

--- mastermind/lib/codemaker.rb
+++ mastermind/lib/codemaker.rb
@@ -3,9 +3,29 @@ class Codemaker
     @secret_code = generate_code
   end
 
+  def return_feedback(codebreaker_guess)
+    feedback = Array.new
+
+    compare_guess_to_secret_code(codebreaker_guess, feedback)
+
+    feedback.shuffle
+  end
+
   private
 
   def generate_code
     [*1..6].sample(4)
+  end
+
+  def compare_guess_to_secret_code(guess, array)
+    guess.each_with_index do |number, index|
+      if number == @secret_code[index]
+        array << 'B'
+      elsif @secret_code.include?(number)
+        array << 'W'
+      else
+        next
+      end
+    end
   end
 end
