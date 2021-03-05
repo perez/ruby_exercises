27ececd107a458c6817e819c967af7409ecb8267
perez <tomwpv@gmail.com>
2021-03-04 20:50:14 -0500

    Add load paths for lib files and update script to run Mastermind on the command line

--- mastermind/bin/main.rb
+++ mastermind/bin/main.rb
@@ -1,19 +1,27 @@
-print "~~~~~~~~~~~~~~~~~~~~~ \n M A S T E R M I N D \n"
-print "~~~~~~~~~~~~~~~~~~~~~ \n  Enter '1' to play  \n"
-print "~~~~~~~~~~~~~~~~~~~~~ \n  Enter '2' to exit  \n"
+require_relative '../lib/game'
+require_relative '../lib/codemaker'
+require_relative '../lib/codebreaker'
+
+print "~~~~~~~~~~~~~~~~~~~~~\n M A S T E R M I N D\n"
+print "~~~~~~~~~~~~~~~~~~~~~\n  Enter '1' to play\n"
+print "~~~~~~~~~~~~~~~~~~~~~\n  Enter '2' to exit\n"
 print "~~~~~~~~~~~~~~~~~~~~~\n> "
 
 loop do
-  case $stdin.gets.strip.downcase
+  case $stdin.gets.strip
   when '1'
-    puts 'Playing!'
+    puts '~~~~~~~~~~~~~~~~~~~~~'
 
     break
   when '2'
-    puts 'Goodbye!'
-
-    break
+    exit
   else
-    print "Invalid input! >:^(\n> "
+    print "Invalid input!\n> "
   end
 end
+
+computer = Codemaker.new
+player = Codebreaker.new
+
+mastermind = Game.new(computer, player)
+mastermind.play
