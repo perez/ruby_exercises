fbe62a08a85c618310cdee533ed1918ab0d28e60
perez <tomwpv@gmail.com>
2021-03-04 20:42:19 -0500

    Add #play and the methods required to be able to play a game of Mastermind

--- mastermind/lib/game.rb
+++ mastermind/lib/game.rb
@@ -6,4 +6,36 @@ class Game
     @codebreaker_guess = nil
     @codemaker_feedback = nil
   end
+
+  def play
+    until game_over?
+      codebreaker_turn
+
+      @guesses_left -= 1
+
+      codemaker_turn
+    end
+
+    result_of_game
+  end
+
+  private
+
+  def game_over?
+    @codemaker_feedback == %w[B B B B] || @guesses_left.zero?
+  end
+
+  def codebreaker_turn
+    @codebreaker_guess = @codebreaker.take_guess
+  end
+
+  def codemaker_turn
+    @codemaker_feedback = @codemaker.return_feedback(@codebreaker_guess)
+
+    puts "Feedback: #{@codemaker_feedback.join(' ')}"
+  end
+
+  def result_of_game
+    puts @codemaker_feedback == %w[B B B B] ? 'Codebreaker wins!' : 'Codebreaker loses!'
+  end
 end
